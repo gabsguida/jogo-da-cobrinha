02710d503447606879255b8ba5ddfd7ac5caeb44
Gabriela Guida <gabrielaguida7@gmail.com>
2020-03-16 19:13:21 -0300

    criação do end game

--- script.js
+++ script.js
@@ -60,7 +60,14 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
-        
+    
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        };
+    };
+
     criarBG();
     criarCobrinha();
     drawFood();
