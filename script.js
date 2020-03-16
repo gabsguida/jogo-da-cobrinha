ce30cb050ee08081058b790ef2af5f154bbc5817
Gabriela Guida <gabrielaguida7@gmail.com>
2020-03-16 18:40:35 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -9,6 +9,15 @@ snake[0] = {  // posição inicial da cobrinha
 };
 
 let direction = "right";
+let food = {        
+    x:Math.floor(Math.random() * 15 + 1) * box,
+    y:Math.floor(Math.random() * 15 + 1) * box
+};
+                    //Math.floor arredonda o numero pra baixo; 
+                    //Math.ceil arredonda pra cima; 
+                    //Math.round arredenda pro mais próximo; 
+                    //Math.random escolhe um número aleatório entre 0 e 1
+                    //Math é uma biblioteca de funções matemáticas
 
 function criarBG(){
     context.fillStyle= "lightgreen"
@@ -18,8 +27,13 @@ function criarBG(){
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
         context.fillStyle= "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box); // 
+        context.fillRect(snake[i].x, snake[i].y, box, box); 
     }           
+};
+
+function drawFood(){
+    context.fillStyle= "red";
+    context.fillRect(food.x, food.y, box, box); //coordenadas da comidinha
 };
 
 document.addEventListener("keydown", update);
@@ -32,17 +46,18 @@ function update(event){   // chama a função update e nesta função há um obj
     if(event.keyCode == 39 && direction != "left") direction ="right";
     if(event.keyCode == 40 && direction != "up") direction = "down";
    
-   document.getElementById("keydown_log").innerHTML=event.key + " (" + event.keyCode + ")";
+   document.getElementById("keydown_log").innerHTML= event.key + " (" + event.keyCode + ")";
 }
 
 function iniciarJogo(){     
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16*box;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
         
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
